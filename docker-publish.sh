3c256be27385b7b2452b3a797e8f8d58132283d2
Aleksey Koloskov <vsyscoder@gmail.com>
2020-04-27 04:18:29 +0300

    Try to fix docker image push stage

--- docker-publish.sh
+++ docker-publish.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -eu
 
-env
+DOCKER_USERNAME="$1"
+DOCKER_PASSWORD="$2"
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker tag ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} ${DOCKER_IMAGE_NAME}:${TRAVIS_TAG}-${TRAVIS_BUILD_NUMBER}
