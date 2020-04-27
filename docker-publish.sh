456d51153c95647e8c16187e515d4afe6396568f
Aleksey Koloskov <vsyscoder@gmail.com>
2020-04-27 04:04:03 +0300

    Add debug info to docker-publish.sh

--- docker-publish.sh
+++ docker-publish.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eu
 
+env
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker tag ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} ${DOCKER_IMAGE_NAME}:${TRAVIS_TAG}-${TRAVIS_BUILD_NUMBER}
 docker tag ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} ${DOCKER_IMAGE_NAME}:latest
