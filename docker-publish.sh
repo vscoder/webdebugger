c713ed81e0c40026170191d731a2f971298cc78d
Aleksey Koloskov <vsyscoder@gmail.com>
2023-06-06 00:23:37 +0300

    Disable travis ci

--- docker-publish.sh
+++ docker-publish.sh
@@ -2,7 +2,7 @@
 set -eux
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker tag ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} ${DOCKER_IMAGE_NAME}:${TRAVIS_TAG}
+docker tag ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
 docker tag ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} ${DOCKER_IMAGE_NAME}:latest
-docker push ${DOCKER_IMAGE_NAME}:${TRAVIS_TAG}
+docker push ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
 docker push ${DOCKER_IMAGE_NAME}:latest
